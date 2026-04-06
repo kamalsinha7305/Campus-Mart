9cea2e67bc50c7303f196689c8fb779c2a8a56a8
kamal <kamalsinha7305@gmail.com>
2026-04-06 19:49:35 +0530

    added getuserProfile controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,33 @@
 import userModel from "../models/User.model.js";
-import sendEmail from "../config/sendEmail.js";
-import bcrypt from "bcrypt";
-import crypto from "crypto";
-import verifyEmailTempplate from "../utils/templates/verifyEmailTemplate.js";
 
+export const getUserProfile = async (req, res) => {
+
+    try {
+        const userId = req.userId;
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User Not Found",
+                success: false,
+                error: true,
+            });
+        }
+        return res.status(200).json({
+            message: "User Profile Fetched Successfully",
+            success: true,
+            error: false,
+            user: user
+        });
+    }
+    catch (err) {
+        console.error("Error fetching in getUserProfile", err);
+        return res.status(500).json({
+            message: err.message || err,
+            success: false,
+            error: true
+        });
+
+    }
+
+};
